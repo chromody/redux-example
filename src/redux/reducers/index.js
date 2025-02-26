c866956cc576e5b938411a273bad96ceeb78ad10
chromody <chromody@gmail.com>
2025-02-25 21:43:25 -0600

    removed dead code

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// src/redux/reducers/index.js
-import { combineReducers } from 'redux';
-import counterReducer from './counterReducer'; // Your individual reducer
-
-const rootReducer = combineReducers({
-  counter: counterReducer,
-});
-
-export default rootReducer;
